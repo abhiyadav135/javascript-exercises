cfde56fe9970e5c09ebe686be852dd31a89517e7
abhiyadav135 <abhiyadav010305@gmail.com>
2025-07-01 16:25:53 +0530

    Updated multiple exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // Reverse the cleaned string
+    const reversed = cleaned.split('').reverse().join('');
+    // Check if cleaned string equals reversed
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
